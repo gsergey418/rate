3d1f612cc839f4079f70d1e8a7b696633854532a
Sergey Gorbunov <gsergey418@yandex.ru>
2024-06-19 11:26:59 +0300

    Добавлена базовый функционал.

--- rate/views.py
+++ rate/views.py
@@ -1,7 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+import requests
+
 
 @api_view(["GET"])
 def get_rate(request):
-    return Response()
+    exchange_api_response = requests.get(
+        "https://open.er-api.com/v6/latest/" + request.GET["from"])
+    exchange_api_json = exchange_api_response.json()
+    result = exchange_api_json["rates"][request.GET["to"]
+                                        ] * int(request.GET["value"])
+    return Response({
+        "result": result
+    })
