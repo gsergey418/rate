afdd0e88e301eaf1f8c141771cf77d6832fcc7ef
Sergey Gorbunov <gsergey418@yandex.ru>
2024-06-19 11:54:18 +0300

    Добавлена валидация и error handling.

--- rate/views.py
+++ rate/views.py
@@ -1,16 +1,33 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.forms import CharField, IntegerField
+from django.core.exceptions import ValidationError
 
 import requests
+from requests.exceptions import RequestException
 
 
 @api_view(["GET"])
 def get_rate(request):
-    exchange_api_response = requests.get(
-        "https://open.er-api.com/v6/latest/" + request.GET["from"])
+
+    try:
+        from_cur = CharField(min_length=3, max_length=3).clean(
+            request.GET["from"]).upper()
+        to_cur = CharField(min_length=3, max_length=3).clean(
+            request.GET["to"]).upper()
+        amount = IntegerField().clean(request.GET["value"])
+    except ValidationError:
+        return Response({"message": "Необходимые параметры: from, to, value"}, status=400)
+
+    try:
+        exchange_api_response = requests.get(
+            f"https://open.er-api.com/v6/latest/{from_cur}")
+    except RequestException:
+        return Response({"message": "Не получилось сделать запрос к стороннему сервису."}, status=504)
+
     exchange_api_json = exchange_api_response.json()
-    result = exchange_api_json["rates"][request.GET["to"]
-                                        ] * int(request.GET["value"])
+    result = exchange_api_json["rates"][to_cur] * amount
+
     return Response({
         "result": result
     })
